2fe3a0694763f1d233a1c12f33641546aecefb88
Yaroslav Mestkovsky <mestkovskij@obt-vlg.ru>
2025-06-06 16:20:59 +0300

    # 52. Отладка.

--- browser_automation.py
+++ browser_automation.py
@@ -112,6 +112,9 @@ class BrowserAutomation:
             time_to_proceed: Предыдущий элемент запускает долгую загрузку, а это время ожидания
         """
         if wait_for:
+            if time_to_proceed:
+                self.logger.info(f'Ожидание завершения операции в течении {time_to_proceed / 1000}')
+
             await self._wait_for_element(selector, time_to_proceed)
         
         if isinstance(selector, list):
